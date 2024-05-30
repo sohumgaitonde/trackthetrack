4d886c55e27f276c10843353b45676f4e366e3d5
elijahmccauley <emmccauley6@gmail.com>
2024-05-30 14:15:55 -0500

    Add row of videos with scroll buttons

--- trackthetrack/app/athletes/teare/page.tsx
+++ trackthetrack/app/athletes/teare/page.tsx
@@ -6,10 +6,22 @@ import Link from 'next/link';
 import logo from '../../assets/ttt1.png';
 import Image from 'next/image';
 import teare from '../../assets/teare.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 
 const TearePage: React.FC = () => {
-  
+  const urls = ['https://www.youtube.com/embed/OMNgKw566u8', 'https://www.youtube.com/embed/eVoQjQ5wR2A', 'https://www.youtube.com/embed/qQvI1f3GqTE', 'https://www.youtube.com/embed/hSM5zbqU7e8', 'https://www.youtube.com/embed/6_lTXHvTvIA', 'https://www.youtube.com/embed/dO41zkMOCGw']
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - 3, 0));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => Math.min(prevIndex + 3, urls.length - 3));
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <Image src={logo} alt="Logo" width={128} height={128} />
@@ -30,17 +42,39 @@ const TearePage: React.FC = () => {
         </h3>
         <Image src={teare} alt="Logo" width={128} height={128} />
       </header>
-      <div className="flex flex-col items-center justify-center h-screen">
-          <iframe
-            className="w-full h-full"
-            src="https://www.youtube.com/embed/OMNgKw566u8"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
-      
-    
+      <div className="flex flex-col items-center">
+
+      <div className="flex space-x-4 mb-4">
+      <button
+          onClick={handlePrev}
+          disabled={currentIndex === 0}
+          className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50 h-1/2 my-14"
+        >
+          <FontAwesomeIcon icon={faArrowLeft} size="lg" />
+        </button>
+        {urls.slice(currentIndex, currentIndex + 3).map((url, index) => (
+          <div key={index} className="w-full max-w-md">
+            <div className="aspect-w-16 aspect-h-9">
+              <iframe
+                className="w-full h-full"
+                src={url}
+                title={`YouTube video player ${index + 1}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        ))}
+        <button
+          onClick={handleNext}
+          disabled={currentIndex >= urls.length - 3}
+          className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50 h-1/2 my-14"
+        >
+          <FontAwesomeIcon icon={faArrowRight} size="lg" />
+        </button>
+      </div>
+    </div>
     </div>
   );
 };
