8f817633704f41b1b83b69a33c25aa39b295535f
elijahmccauley <emmccauley6@gmail.com>
2024-06-05 13:34:54 -0400

    Add link to cooper teare instagram on his page, made base team pages

---
+++ trackthetrack/app/teams/nike/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import 'tailwindcss/tailwind.css';
+import Link from 'next/link';
+import logo from '../../assets/ttt1.png';
+import Image from 'next/image';
+import nike from '../../assets/teams/nike.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+
+const NikePage: React.FC = () => {
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      
+      <header className="mb-12">
+        <h1 className="text-5xl font-extrabold text-center text-blue-700 mb-4">
+        <Image src={logo} alt="Logo" width={128} height={128} />
+          <Link href="/">
+            <button >
+              Track the Track
+            </button>
+          </Link>
+          <Image src={nike} alt="Logo" width={128} height={128} />
+        </h1>
+        <h2 className="text-3xl font-mono text-center text-blue-500">
+          Road to the Olympics
+        </h2>
+        <h3 className="text-3xl font-mono text-center text-blue-500">
+          Nike
+        </h3>
+      </header>
+      <div className="flex flex-col items-center">
+
+      <div className="flex space-x-4 mb-4">
+      
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default NikePage;
