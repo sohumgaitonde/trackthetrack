9de9aa93bd1dc71e373aebd6bbac2fe42f77a5f4
elijahmccauley <emmccauley6@gmail.com>
2024-07-30 01:56:31 -0400

    make scraper get entries for all 4 key events

--- trackthetrack/app/olympic1500.py
+++ trackthetrack/app/olympic1500.py
@@ -30,13 +30,84 @@ for row in athlete_rows:
     athlete_name = row.find('span', {'class': 'competitor-long-name'}).text.strip()
     names.append(athlete_name)
     nationalities.append(nation_abbr)
-athletics_dict = {
+athletics_dict_1500 = {
     'Athlete':names,
     'Nationality':nationalities
     }
 
-olympic1500_df = pd.DataFrame(athletics_dict)
-olympic1500_df.to_json('olympic1500m.json', orient='records')
-    
+driver = webdriver.Chrome(options=chrome_options)
+url = 'https://olympics.com/en/paris-2024/entries/athletics/all-noc/men-s-800m'
+driver.get(url)
+time.sleep(6)
+html_content = driver.page_source
+soup = bs(html_content, 'html.parser')
+driver.quit()
+athlete_rows = soup.find_all('tr', {'data-key': lambda x: x and 'mirs-table-athletes' in x})
+#print(athlete_rows)
+names = []
+nationalities = []
+for row in athlete_rows:
+    print(row)
+    nation_abbr = row.find('div', {'class': 'wrsNoc'}).text.strip()
+    athlete_name = row.find('span', {'class': 'competitor-long-name'}).text.strip()
+    names.append(athlete_name)
+    nationalities.append(nation_abbr)
+athletics_dict_800 = {
+    'Athlete':names,
+    'Nationality':nationalities
+    }
 
-print(f"Number of rows found: {len(athlete_rows)}")
+driver = webdriver.Chrome(options=chrome_options)
+url = 'https://olympics.com/en/paris-2024/entries/athletics/all-noc/men-s-5000m'
+driver.get(url)
+time.sleep(6)
+html_content = driver.page_source
+soup = bs(html_content, 'html.parser')
+driver.quit()
+athlete_rows = soup.find_all('tr', {'data-key': lambda x: x and 'mirs-table-athletes' in x})
+#print(athlete_rows)
+names = []
+nationalities = []
+for row in athlete_rows:
+    print(row)
+    nation_abbr = row.find('div', {'class': 'wrsNoc'}).text.strip()
+    athlete_name = row.find('span', {'class': 'competitor-long-name'}).text.strip()
+    names.append(athlete_name)
+    nationalities.append(nation_abbr)
+athletics_dict_5000 = {
+    'Athlete':names,
+    'Nationality':nationalities
+    }
+
+driver = webdriver.Chrome(options=chrome_options)
+url = 'https://olympics.com/en/paris-2024/entries/athletics/all-noc/men-s-10000m'
+driver.get(url)
+time.sleep(6)
+html_content = driver.page_source
+soup = bs(html_content, 'html.parser')
+driver.quit()
+athlete_rows = soup.find_all('tr', {'data-key': lambda x: x and 'mirs-table-athletes' in x})
+#print(athlete_rows)
+names = []
+nationalities = []
+for row in athlete_rows:
+    print(row)
+    nation_abbr = row.find('div', {'class': 'wrsNoc'}).text.strip()
+    athlete_name = row.find('span', {'class': 'competitor-long-name'}).text.strip()
+    names.append(athlete_name)
+    nationalities.append(nation_abbr)
+athletics_dict_10000 = {
+    'Athlete':names,
+    'Nationality':nationalities
+    }
+
+
+all_athletes = {
+    '800': athletics_dict_800,
+    '1500': athletics_dict_1500,
+    '5000': athletics_dict_5000,
+    '10000': athletics_dict_10000
+    }
+
+olympic_df = pd.DataFrame(all_athletes)
+olympic_df.to_json('olympic_athletes.json', orient='records')
